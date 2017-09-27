4cbf6676b645d58a6f06278e032071f8c8d2a3df
Luan Marques <soluan31@hotmail.com>
2017-09-27 18:01:08 -0300

    finalizando fileupload

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, request, redirect, url_for, abort, session, jsonify, flash
+from flask import Flask, render_template, request, redirect, url_for, abort, session, jsonify, flash, \
+    send_from_directory
+import os
 from functools import wraps
 from flask_mysqldb import MySQL
 
@@ -9,6 +11,8 @@ SQL_ATUALIZA_JOGO = 'UPDATE jogo SET nome=%s, categoria=%s, console=%s where id 
 SQL_BUSCA_JOGOS = 'SELECT id, nome, categoria, console from jogo'
 SQL_CRIA_JOGO = 'INSERT into jogo (nome, categoria, console) values (%s, %s, %s)'
 
+RAIZ_APP = os.path.dirname(os.path.abspath(__file__))
+
 app = Flask(__name__, static_folder='static', template_folder='templates')
 
 # é necessário um secret_key para que a sessão da aplicação não fique acessível para qualquer um
@@ -18,6 +22,8 @@ app.config['MYSQL_USER'] = "root"
 app.config['MYSQL_PASSWORD'] = "admin"
 app.config['MYSQL_DB'] = "jogoteca"
 app.config['MYSQL_PORT'] = 3306
+app.config['UPLOAD_FOLDER'] = RAIZ_APP + '/uploads'
+
 
 db = MySQL(app)
 
@@ -30,13 +36,16 @@ class Jogo:
         self.console = console
 
     def save(self):
+        cursor = db.connection.cursor()
         if self.id:
-            db.connection.cursor().execute(SQL_ATUALIZA_JOGO,
+            cursor.execute(SQL_ATUALIZA_JOGO,
                                            (self.nome, self.categoria, self.console, self.id))
         else:
-            db.connection.cursor().execute(SQL_CRIA_JOGO,
+            cursor.execute(SQL_CRIA_JOGO,
                                            (self.nome, self.categoria, self.console))
+            self.id = cursor.lastrowid
         db.connection.commit()
+        return self
 
     @staticmethod
     def get_all():
@@ -64,6 +73,14 @@ class Jogo:
     def delete(id):
         db.connection.cursor().execute(SQL_DELETA_JOGO, (id,))
         db.connection.commit()
+        deleta_foto(id)
+
+
+def deleta_foto(jogo_id):
+    foto = 'foto{id}.jpg'.format(id=jogo_id)
+    pasta = os.listdir(app.config['UPLOAD_FOLDER'])
+    if foto in pasta:
+        os.remove(os.path.join(app.config['UPLOAD_FOLDER'], foto))
 
 
 class Usuario:
@@ -80,17 +97,18 @@ class Usuario:
         return Usuario(tupla[0], tupla[1], tupla[2])
 
     @staticmethod
-    def get(identificador):
+    def get(id):
         cursor = db.connection.cursor()
-        cursor.execute(SQL_USUARIO_POR_ID, (identificador,))
-        usuario = Usuario.traduz_usuario(cursor.fetchone())
+        cursor.execute(SQL_USUARIO_POR_ID, (id,))
+        dados = cursor.fetchone()
+        usuario = Usuario.traduz_usuario(dados) if dados else None
         return usuario
 
 
 def protegida(f):
     @wraps(f)
     def valida_usuario(*args, **kwargs):
-        if session['usuario_logado'] is None:
+        if 'usuario_logado' not in session or session['usuario_logado'] is None:
             return redirect(url_for('login', proxima=request.url))
         return f(*args, **kwargs)
     return valida_usuario
@@ -137,8 +155,18 @@ def novo():
 @protegida
 def criar():
     novo_jogo = Jogo(request.form['nome'], request.form['categoria'], request.form['console'])
-    novo_jogo.save()
-    flash('Jogo criado com sucesso!')
+    jogo_salvo = novo_jogo.save()
+    message = 'Jogo criado com sucesso!'
+    if 'nome_arquivo' in request.files:
+        arquivo = request.files['nome_arquivo']
+        if arquivo.filename != '':
+            extensao = arquivo.filename.rsplit('.', 1)[1].lower()
+            if extensao != 'jpg':
+                message = 'Jogo criado , mas formato da foto deve ser jpg.'
+            else:
+                arquivo.save(os.path.join(app.config['UPLOAD_FOLDER'],
+                                          'foto{id}.jpg'.format(id=jogo_salvo.id)))
+    flash(message)
     return redirect(url_for('index'))
 
 
@@ -147,9 +175,7 @@ def criar():
 def editar(id):
     categorias = ['RPG', 'Ação', 'FPS', 'Indie', 'Esporte']
     jogo = Jogo.get(id)
-    print(jogo)
-    print(jogo.categoria)
-    return render_template('editar.html', jogo=jogo,  categorias=categorias)
+    return render_template('editar.html', jogo=jogo, categorias=categorias)
 
 
 @app.route('/atualizar', methods=['GET', 'POST'])
@@ -157,14 +183,30 @@ def editar(id):
 def atualizar():
     jogo = Jogo(request.form['nome'], request.form['categoria'], request.form['console'], id=request.form['id'])
     jogo.save()
-    flash('Jogo atualizado com sucesso!')
+    message = 'Jogo atualizado com sucesso!'
+    if 'nome_arquivo' in request.files:
+        arquivo = request.files['nome_arquivo']
+        if arquivo.filename != '':
+            extensao = arquivo.filename.rsplit('.', 1)[1].lower()
+            if extensao != 'jpg':
+                message = 'Jogo atualizado , mas formato da foto deve ser jpg.'
+            else:
+                arquivo.save(os.path.join(app.config['UPLOAD_FOLDER'],
+                                          'foto{id}.jpg'.format(id=jogo.id)))
+    flash(message)
     return redirect(url_for('index'))
 
 
-@app.route('/deletar/<int:identificador>')
-def deletar(identificador):
-    Jogo.delete(int(identificador))
-    return jsonify(id=identificador)
+@app.route('/deletar/<int:id>')
+def deletar(id):
+    Jogo.delete(int(id))
+    return jsonify(id=id)
+
+
+@app.route('/uploads/<nome_arquivo>')
+@app.route('/uploads/<nome_arquivo>')
+def imagem(nome_arquivo):
+    return send_from_directory('uploads', nome_arquivo)
 
 
 @app.errorhandler(404)
--- app/static/js/app.js
+++ app/static/js/app.js
@@ -8,6 +8,25 @@ $('.remover').click(event => {
     )
     .fail(err => {
         console.log(err)
-        alert('Não foi possível concluir a requisição')
+        alert('Não foi possível remover este jogo.')
     });
+});
+
+
+$('form input[type="file"]').change(event => {
+  console.log(event.target.files)
+  let arquivos = event.target.files;
+  if(arquivos.length === 0) {
+    console.log('sem imagem pra mostrar')
+    // adicionar imagem default
+  } else {
+      if(arquivos[0].type == 'image/jpeg') {
+        $('#foto_jogo').remove();
+        let imagem = $('<img id="foto_jogo" class="img-responsive">');
+        imagem.attr('src', window.URL.createObjectURL(arquivos[0]));
+        $('figure').prepend(imagem);
+      } else {
+        alert('Formato não suportado')
+      }
+  }
 });
